e0838ebac4f0eb3a4ce90c5488801e533c916944
Ieva Stripkāne <ieva.stripkane.2002@gmail.com>
2020-05-06 17:40:29 +0300

    17:40

---
+++ CalculatorSimple.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorSimple
+{
+    public partial class Calculator : Form
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+            InitializeCalculator();
+        }
+
+        private void InitializeCalculator()
+        {
+            this.BackColor = Color.Purple;
+
+            string buttonName = null;
+            Button button = null;
+            for (int i = 0; i < 10; i++)
+            {
+                buttonName = "button" + i;
+                button = (Button)this.Controls[buttonName];
+            }
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+      
+
+
+
+} 
